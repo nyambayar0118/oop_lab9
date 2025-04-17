0e2cd92fbec9d7f69908d06491a5175a8ab38e28
Tuugi <tuugiienkhbayr@gmail.com>
2025-04-17 20:08:49 +0800

    bug fixes

--- classes/Spouse.h
+++ classes/Spouse.h
@@ -3,9 +3,8 @@
 
 // Эхнэр/Нөхөр класс
 class Spouse: public Person {
-<<<<<<< HEAD
-ds;
-=======
+ Date AnniversaryDate;
+
    // Гишүүн өгөгдөл
    protected:
    Date anniversaryDate;
@@ -15,10 +14,6 @@ ds;
     Date getAnniversaryDate();
     // Огноог тохируулах функц
     void setAnniversaryDate( int y,int m,int d);
-<<<<<<< HEAD
->>>>>>> b718aac368ba87000f8c01b6515d23bc7f7b54f5
-=======
     // Spouse Конструктор
     Spouse(int y,int m,int d);
->>>>>>> 3f405ac4bc891ab527f043ada82c949ad63b0e81
 };
