b718aac368ba87000f8c01b6515d23bc7f7b54f5
Tuugi <tuugiienkhbayr@gmail.com>
2025-04-17 18:55:44 +0800

    Spouse  class iin todorhoiloltiig bihcij  get and set function iig heregjuulj duussan

--- classes/Spouse.h
+++ classes/Spouse.h
@@ -3,5 +3,13 @@
 
 // Эхнэр/Нөхөр класс
 class Spouse: public Person {
-
+   // Гишүүн өгөгдөл
+   protected:
+   Date anniversaryDate;
+   // Гишүүн функцүүд
+    public:
+    // Огноог буцаах функц
+    Date getAnniversaryDate();
+    // Огноог тохируулах функц
+    void setAnniversaryDate( int y,int m,int d);
 };
