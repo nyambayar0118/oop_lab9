8860a7c8c671fe8cf570446910b748dc5a2d49b6
Tuugi <tuugiienkhbayr@gmail.com>
2025-04-18 08:58:18 +0800

    imp code organized, fixed few mistakes, added missing stuff

--- imp.cpp
+++ imp.cpp
@@ -11,179 +11,239 @@
 using namespace std;
 
 // Child классын гишүүн функцүүд
-char* Child::getFavoriteToy() {
+char *Child::getFavoriteToy()
+{
     return favoriteToy;
 }
-void Child::setFavoriteToy(char* toy) {
+void Child::setFavoriteToy(char *toy)
+{
+    delete[] favoriteToy;
     favoriteToy = new char[strlen(toy) + 1];
     strcpy(favoriteToy, toy);
 }
-// Date классын гишүүн функцүүд
+Child::Child(char *n, char *ss, int a, char *t) : Person(n, ss, a)
+{
+    setFavoriteToy(t);
+}
+Child::~Child()
+{
+    delete[] favoriteToy;
+}
 
-//get function
-int Date::getYear() {
+
+
+
+// Date классын гишүүн функцүүд
+int Date::getYear()
+{
     return year;
 }
-int Date::getMonth() {
+int Date::getMonth()
+{
     return month;
 }
-int Date::getDay() {
+int Date::getDay()
+{
     return day;
 }
-//set function
-void Date::setYear(int y) {
+void Date::setYear(int y)
+{
     year = y;
 }
-void Date::setMonth(int m) {
+void Date::setMonth(int m)
+{
     month = m;
 }
-void Date::setDay(int d) {
+void Date::setDay(int d)
+{
     day = d;
 }
-// Division классын гишүүн функцүүд
+Date::Date(int y, int m, int d)
+{
+    year = y;
+    month = m;
+    day = d;
+}
 
-//get function
-char* Division::getDivisionName() {
+
+
+
+
+
+// Division классын гишүүн функцүүд
+char *Division::getDivisionName()
+{
     return divisionName;
 }
-//set function
-void Division::setDivisionName(char* name) {
+void Division::setDivisionName(char *name)
+{
+    delete[] divisionName;
     divisionName = new char[strlen(name) + 1];
     strcpy(divisionName, name);
 }
-
-// Employee классын гишүүн функцүүд
-
-//get function
-char* Employee::getCompanyID() {
-    return companyID;
-}
-char* Employee::getTitle(){
-    return title;
-}
-Date Employee::getStartDate(){
-    return startDate;
-}
-Spouse* Employee::getSpouse{
-    return Spouse;
-}
-vector<Child*> Employee::getChildren(){
-    return children;
-}
-vector<JobDescription*> Employee::getjds(){
-    return jds;
-}
-Division* Employee::getDiv(){
-    return div;
-}
-//set function
-void Employee::setCompanyID(char* id){
-    companyID = new char[strlen(id)+1];
-    strcpy(companyID, id);
-}
-void Employee::setTitle(char* tit){
-    title = new char[strlen(tit)+1];
-    strcpy(title, tit);
-}
-void Employee::setStartDate(int y, int m, int d){
-    startDate.setYear(y);
-    startDate.setMonth(m);
-    startDate.setDay(d);  
-}
-// JobDescription классын гишүүн функцүүд
-
-char* JobDescription::getDescription() {
-    return description;
-}
-void JobDescription::setDescription(char* desc) {
-    description = new char[strlen(desc) + 1];
-    strcpy(description, desc);
-}
-
-// Person классын гишүүн функцүүд
-
-char* Person::getName() {
-    return name;
-}
-char* Person::getSSNum() {
-    return SSNum;
-}
-int Person::getAge() {
-    return age;
-}
-void Person::setName(char* n) {
-    name = new char[strlen(n) + 1];
-    strcpy(name, n);
-}
-
-void Person::setAge(int a) {
-
-    age = a;
-}
-void Person::setSSNum(char* ss) {
-    SSNum = new char[strlen(ss) + 1];
-    strcpy(SSNum, ss);
-}
-// Spouse классын гишүүн функцүүд
-
-//get function
-Date Spouse::getAnniversaryDate() {
-    return anniversaryDate;
-}
-//set function
-void Spouse::setAnniversaryDate(int y, int m, int d) {
-    anniversaryDate.setYear (y);
-    anniversaryDate.setMonth(m);
-    anniversaryDate.setDay(d);
-}
-
-//consturctor
-
-//date constructor
-Date::Date(int y, int m, int d) {
-    year = y;
-    month = m;
-    day = d;
-}
-
-//Division parametrtei constructor
-Division::Division(char* name) {
+Division::Division(char *name)
+{
     setDivisionName(name);
 }
-//Division destructor
-Division::~Division() {
+Division::~Division()
+{
     delete[] divisionName;
 }
 
-//Spouse constructor
-Spouse::Spouse(int y,int m,int d){
-    Date(y,m,d);
+
+
+
+
+
+
+// Employee классын гишүүн функцүүд
+char *Employee::getCompanyID()
+{
+    return companyID;
 }
-//JobDescription constructor
-JobDescription::JobDescription(char* desc) {
+char *Employee::getTitle()
+{
+    return title;
+}
+Date Employee::getStartDate()
+{
+    return startDate;
+}
+Spouse *Employee::getSpouse()
+{
+    return Spouse;
+}
+vector<Child *> Employee::getChildren()
+{
+    return children;
+}
+vector<JobDescription *> Employee::getjds()
+{
+    return jds;
+}
+Division *Employee::getDiv()
+{
+    return div;
+}
+void Employee::setCompanyID(char *id)
+{
+    delete[] companyID;
+    companyID = new char[strlen(id) + 1];
+    strcpy(companyID, id);
+}
+void Employee::setTitle(char *tit)
+{
+    delete[] title;
+    title = new char[strlen(tit) + 1];
+    strcpy(title, tit);
+}
+void Employee::setStartDate(int y, int m, int d)
+{
+    startDate.setYear(y);
+    startDate.setMonth(m);
+    startDate.setDay(d);
+}
+Employee::Employee(char *n, char *ss, int a, char *id, char *tit, int y, int m, int d) : Person(n, ss, a)
+{
+    setCompanyID(id);
+    setTitle(tit);
+    setStartDate(y, m, d);
+}
+Employee::~Employee() {
+    delete[] companyID;
+    delete[] title;
+    delete spouse;
+    for (auto child : children) delete child;
+    for (auto jd : jds) delete jd;
+}
+
+
+
+
+
+// JobDescription классын гишүүн функцүүд
+char *JobDescription::getDescription()
+{
+    return description;
+}
+void JobDescription::setDescription(char *desc)
+{
+    delete[] description;
+    description = new char[strlen(desc) + 1];
+    strcpy(description, desc);
+}
+JobDescription::JobDescription(char *desc)
+{
     setDescription(desc);
 }
-//JobDescription descturctor
-JobDescription::~JobDescription() {
+JobDescription::~JobDescription()
+{
     delete[] description;
 }
-//Person constructor
-Person::Person(char* n, char* ss, int a) {
+
+
+
+
+
+// Person классын гишүүн функцүүд
+char *Person::getName()
+{
+    return name;
+}
+char *Person::getSSNum()
+{
+    return SSNum;
+}
+int Person::getAge()
+{
+    return age;
+}
+void Person::setName(char *n)
+{
+    delete[] name;
+    name = new char[strlen(n) + 1];
+    strcpy(name, n);
+}
+void Person::setAge(int a)
+{
+    age = a;
+}
+void Person::setSSNum(char *ss)
+{
+    delete[] SSNum;
+    SSNum = new char[strlen(ss) + 1];
+    strcpy(SSNum, ss);
+}
+Person::Person(char *n, char *ss, int a)
+{
     setName(n);
     setSSNum(ss);
     setAge(a);
 }
-//Person destructor
-Person::~Person() {
+Person::~Person()
+{
     delete[] name;
     delete[] SSNum;
 }
 
-//Child contructor
-Child::Child(char* n, char* ss, int a, char* t) :Person(n,ss,a) {
-setFavoriteToy(t);
+
+
+
+
+
+
+// Spouse классын гишүүн функцүүд
+Date Spouse::getAnniversaryDate()
+{
+    return anniversaryDate;
 }
-//Child destructor
-Child::~Child(){
-    delete[] favoriteToy;
+void Spouse::setAnniversaryDate(int y, int m, int d)
+{
+    anniversaryDate.setYear(y);
+    anniversaryDate.setMonth(m);
+    anniversaryDate.setDay(d);
+}
+Spouse::Spouse(int y, int m, int d)
+{
+    setAnniversaryDate(y, m, d);
 }
